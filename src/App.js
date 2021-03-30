3ce990658df8d0d614d7d460e8afe9e912cbceac
Yu Chu <cyjeff@gmail.com>
2021-03-30 23:24:34 +0900

    updated map component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
-import MapSample from "./MapSample";
-=======
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
->>>>>>> 46e77dbbb76ffa56f35fc5e095e275b2c4e34976
 
 //components
 import Map from './components/Map';
@@ -18,28 +12,10 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <MapSample id="map" />
-=======
       <Map />
       <Info />
       <Navbar />
       <Rating />
->>>>>>> 46e77dbbb76ffa56f35fc5e095e275b2c4e34976
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,91 @@
-import React from 'react';
+import React from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  DirectionsService,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
-function Map() {
-    return (
-        <div>
-            Map comes here!
-        </div>
-    )
+const containerStyle = {
+  width: "100%",
+  height: "400px",
 };
 
-export default Map;
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyA9KS5M8qGlyWG5i09UGn4xd1RquRj1O2Y",
+  });
+
+  const [map, setMap] = React.useState(null);
+  const [response, setResponse] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  function directionsCallback(response) {
+    console.log(response);
+
+    if (response !== null) {
+      if (response.status === "OK") {
+        setResponse(response);
+      } else {
+        console.log("response: ", response);
+      }
+    }
+  }
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+
+      {response === null && (
+        <DirectionsService
+          // required
+          options={{
+            destination: "roppongi",
+            origin: "akasaka",
+            waypoints: [
+              { location: { lat: 35.665137, lng: 139.725094 }, stopover: true },
+            ],
+            travelMode: "WALKING",
+          }}
+          // required
+          callback={directionsCallback}
+        />
+      )}
+
+      {response !== null && (
+        <DirectionsRenderer
+          // required
+          options={{
+            directions: response,
+          }}
+        />
+      )}
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(Map);
