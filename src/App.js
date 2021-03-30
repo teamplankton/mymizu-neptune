0561dc0c1948a75de3256ea8ad25af1a54f5d510
Yu Chu <cyjeff@gmail.com>
2021-03-30 23:19:11 +0900

    added test map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MapSample from "./MapSample";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MapSample id="map" />
     </div>
   );
 }
