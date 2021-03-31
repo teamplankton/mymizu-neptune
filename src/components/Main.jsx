b3f869a32919f6907706e4e385de96ca44a22e58
ysm0706glee <ysm0706glee@gmail.com>
2021-03-31 17:01:08 +0900

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "./Map";
 import Info from "./Info";
 import Navbar from "./Navbar";
@@ -6,12 +6,13 @@ import Rating from "./Rating";
 import Route from "./Route";
 
 export default function Main() {
+  const [view, setView] = useState(false);
   return (
     <>
       <div>
         <Map />
       </div>
-      <Navbar />
+      {view ? <Info /> : <Navbar />}
     </>
   );
 }
