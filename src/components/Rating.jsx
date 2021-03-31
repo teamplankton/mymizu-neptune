112d998af726d5b8a8e445b7a2b102fb39a520e5
Yu Chu <cyjeff@gmail.com>
2021-03-31 17:49:21 +0900

    updated css for info and rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Rating() {
-    return (
-        <div>
-            <div>
-                <h3>この場所で給水しますか？</h3>
-                <p>Code Chrysalis Japan</p>
-            </div>
-            <img src="" alt="bottle"/>
-            <div className="rating">
-                <div className="star">
-                    Rate here!
-                </div>
-                <div className="comment">
-                    <textarea></textarea>
-                </div>
-            </div>
-            <button>給水を記録</button>
+  return (
+    <div>
+      <div>
+        <h3>この場所で給水しますか？</h3>
+        <p>Code Chrysalis Japan</p>
+      </div>
+      <img src="" alt="bottle" />
+      <div className="rating">
+        <div className="star">Rate here!</div>
+        <div className="comment">
+          <textarea></textarea>
         </div>
-    );
+      </div>
+      <button className="info-button">給水を記録</button>
+    </div>
+  );
 }
 
 export default Rating;
