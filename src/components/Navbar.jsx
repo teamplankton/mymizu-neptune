efbbc2649b5c659b848745a333f84ce8ba1bc5de
Misako Kondoh <misapipipink@gmail.com>
2021-03-31 16:14:50 +0900

    Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import icon from './mymizu-white-icon.png';
+import iconFlip from './mymizu-white-icon-flip.png';
+import iconTracker from './tracker-icon.png';
+import iconMeatball from './meatball-icon.png';
 
 function Navbar() {
     return (
         <nav id="nav">
 	<ul>
-		<li><a href="/">Refill Map</a></li>
-		<li><a href="/route">Route</a></li>
-		<li><a href="/tracker">Refill Tracker</a></li>
-		<li><a href="/others">Others</a></li>
+		<li><a href="/">
+				<div>
+					<img src={icon} alt="icon" />
+				</div>
+				<div>
+					Refill Map
+				</div>
+			</a>
+		</li>
+		<li><a href="/route">
+				<div>
+					<img src={iconFlip} alt="icon" />
+				</div>
+				<div>
+					Route
+				</div>
+			</a>
+		</li>
+		<li><a href="/tracker">
+				<div>
+					<img src={iconTracker} alt="icon" />
+				</div>
+				<div>
+					Refill Tracker
+				</div>
+			</a>
+		</li>
+		<li><a href="/others">
+				<div>
+					<img src={iconMeatball} alt="icon" />
+				</div>
+				<div>
+					Others
+				</div>
+			</a>
+		</li>
 	</ul>
 </nav>
     );
