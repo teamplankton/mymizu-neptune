2f04016d4ce8aac9b324bef18072dbeeaad2bfb4
Yu Chu <cyjeff@gmail.com>
2021-03-31 23:00:29 +0900

    added marker data link to info page, updated navbar css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,22 @@ import Map from "./Map";
 import Info from "./Info";
 import Navbar from "./Navbar";
 import Rating from "./Rating";
-import Route from "./Route";
 
 export default function Main() {
+  const [taps, setTaps] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
   return (
     <>
       <div>
-        <Map />
+        <Map taps={taps} setTaps={setTaps} setSelected={setSelected} />
       </div>
       <Navbar />
-      <Info />
+      {selected && (
+        <Info
+          selected={taps.filter((i) => i.id === selected)[0]}
+          setSelected={setSelected}
+        />
+      )}
       <Rating />
     </>
   );
