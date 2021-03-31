8416d970ff858e505f2cbab1575bae3531f75508
ysm0706glee <ysm0706glee@gmail.com>
2021-03-31 10:34:20 +0900

    add migrations and seeds

--- Migrations/20210330084352_create_rating_table.js
+++ Migrations/20210330084352_create_rating_table.js
@@ -1,3 +1,16 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("ratings", function (table) {
+    table.increments();
+    table.integer("tap_id");
+    table.integer("star");
+    table.string("comment");
+  });
+};
 
-exports.down = function (knex) {};
+exports.down = function (knex, Promise) {
+  return knex.schema.hasTable("ratings").then(function (exists) {
+    if (exists) {
+      knex.schema.dropTable("ratings");
+    }
+  });
+};
