05b8e41d7c280726b39bfe0a9a4689ecd9913154
bilaalukis <bilaalukis123@gmail.com>
2021-04-01 10:22:49 +0900

    Added stars and working on rating logic

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Rating as Rate } from "@material-ui/lab";
+import Typography from "@material-ui/core/Typography";
+import { InputGroup, FormControl } from "react-bootstrap";
+import Box from "@material-ui/core/Box";
+import axios from "axios";
 
 function Rating() {
+  const [star, setStar] = useState(3);
+  const [comment, setComment] = useState("");
+
+  async function handleRate() {
+    const data = {
+      tap_id: {},
+      star: star,
+      comment: comment,
+    };
+
+    //   await axios.post("/api/rating", data)
+  }
+
   return (
     <div>
-      <div>
-        <h3>この場所で給水しますか？</h3>
-        <p>Code Chrysalis Japan</p>
-      </div>
-      <img src="" alt="bottle" />
-      <div className="rating">
-        <div className="star">Rate here!</div>
-        <div className="comment">
-          <textarea></textarea>
-        </div>
-      </div>
-      <button className="info-button">給水を記録</button>
+      <Box component="fieldset" mb={3} borderColor="transparent">
+        <Typography component="legend">
+          {`How was the water at Code Chrysalis`}{" "}
+        </Typography>
+        <Rate
+          name="simple-controlled"
+          value={star}
+          onChange={(event, newValue) => {
+            setStar(newValue);
+          }}
+        />
+      </Box>
+      <InputGroup>
+        <FormControl
+          //   onChange={handleChange}
+          as="textarea"
+          rows={3}
+          placeholder="Any comments?..."
+        />
+      </InputGroup>
+      <button className="info-button" onClick={handleRate}>
+        給水を記録
+      </button>
     </div>
   );
 }
